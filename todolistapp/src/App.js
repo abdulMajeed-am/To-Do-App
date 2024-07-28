e635ed90664f6e4c4728176532fe9d1518d471e4
Abdul Majeed <er.abdulmajeedm@gmail.com>
2024-07-28 16:35:58 +0530

    Backend JavaScript code added for Operation

--- todolistapp/src/App.js
+++ todolistapp/src/App.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react'
 import TodoApp from './component/TodoApp'
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <TodoApp/>
-      </div>
-    )
-  }
-}
+const App=()=> <div> <TodoApp/></div>;
+export default App;
--- todolistapp/src/component/TodoApp.js
+++ todolistapp/src/component/TodoApp.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react'
 import "./TodoApp.css";
 export default class TodoApp extends Component {
+  state={
+    input:"",
+    items:[]
+  }
+  handleChange= event =>{
+    this.setState({
+        input:event.target.value
+    })
+  }
+
+  storeItems=(event)=>{
+    event.preventDefault();
+    const {input}= this.state;
+    this.setState({
+      items:[...this.state.items,input],
+      input:''
+    })
+    // console.log(this.state.items)
+  }
+  deleteItems=(key)=>{
+      this.setState({
+        items:this.state.items.filter((data,index) => index !==key)
+      })
+  }
   render() {
+    const {input, items}=this.state;
     return (
       <div className='todo-container'>
-        <form className='input-section'>
+        <form className='input-section' onSubmit={this.storeItems}>
             <h1>ToDoApp</h1>
-            <input type="text" placeholder='Enter list element...'/>
+            <input type="text" value={input} placeholder='Enter list element...' onChange={this.handleChange}/>
         </form>
         <ul>
-            <li>
-                Item <i class="fa-solid fa-trash-can"></i>
-            </li>
-            <li>
-                 Item <i class="fa-solid fa-trash-can"></i>
-            </li>
-            <li>
-                  Item <i class="fa-solid fa-trash-can"></i>
-            </li>
+            {items.map((data,index)=>(
+              <li key={index}>
+                  {data}<i className="fa-solid fa-trash-can" onClick={()=> this.deleteItems(index)}></i>
+              </li>
+            ))}
         </ul>
       </div>
     )
